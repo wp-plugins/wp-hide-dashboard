de0ea33df80f0aafe075dc3afaed1b9dad51e140
kpdesign <kpdesign@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-19 16:37:19 +0000

    Updated for 2.7. Added version checking.
    git-svn-id: https://plugins.svn.wordpress.org/wp-hide-dashboard/trunk@81150 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,20 +3,20 @@ Contributors: kpdesign
 Donate link: http://www.kpdesign.net/wp-plugins/wp-hide-dashboard/
 Tags: admin, administration, dashboard, hide
 Requires at least: 2.5
-Tested up to: 2.6.2
-Stable tag: 1.0
+Tested up to: 2.7
+Stable tag: 1.1
 
-Hide the dashboard from your blog subscribers when they are logged in.
+Hide the Dashboard link (2.5+) and Tools menu (2.7) from your blog subscribers when they are logged in.
 
 == Description ==
 
-This plugin removes the dashboard link and prevents access to the dashboard for users assigned to the Subscriber role. This is useful if you want to allow your subscribers to edit their own profiles, but don't want them wandering around the rest of your WordPress admin section.
+This plugin removes the Dashboard link and the Tools menu, and prevents access to the Dashboard for users assigned to the `Subscriber` role. This is useful if you want to allow your subscribers to edit their own profiles, but don't want them wandering around the rest of your WordPress admin section.
 
-Users belonging to any of the other WordPress roles will continue to see the dashboard link and have dashboard access that corresponds to their role's capabilities.
+Users belonging to any of the other WordPress roles will continue to see the Dashboard link and the Tools menu, and have access to the other sections of the WordPress admin that corresponds to their role's capabilities.
 
 Based on the [IWG Hide Dashboard](http://www.im-web-gefunden.de/wordpress-plugins/iwg-hide-dashboard/ "IWG Hide Dashboard") plugin by Thomas Schneider, which requires having the Role Manager plugin activated in order for it to function.
 
-This plugin relies only on core WordPress capabilities to function.
+This plugin relies only on core WordPress capabilities.
 
 == Installation ==
 
@@ -33,7 +33,8 @@ None
 
 == Screenshots ==
 
-1. Screenshot of the upper-left portion of admin section
+1. Screenshot of the upper-left portion of 2.6 admin section
+2. Screenshot of the upper-left portion of 2.7 admin section
 
 == Support ==
 
@@ -41,4 +42,10 @@ Support is provided at http://www.kpdesign.net/wp-plugins/wp-hide-dashboard/
 
 == History ==
 
-Version 1.0: Initial release
+Version 1.1:
+- Added WordPress version checking.
+- Added code for defining path to /wp-content/plugins/ if outside the WordPress directory.
+- Added removal of Tools menu and collapsible arrow from the menu area in 2.7.
+
+Version 1.0:
+- Initial release
--- wp-hide-dashboard.php
+++ wp-hide-dashboard.php
@@ -2,10 +2,10 @@
 /*
 Plugin Name: WP Hide Dashboard
 Plugin URI: http://www.kpdesign.net/wp-plugins/wp-hide-dashboard/
-Description: Simple plugin that removes the dashboard link and prevents dashboard access to users assigned to the <em>Subscriber</em> role. Useful if you allow your subscribers to edit their own profiles, but don't want them wandering around your WordPress admin section. Based on the <a title="IWG Hide Dashboard" href="http://www.im-web-gefunden.de/wordpress-plugins/iwg-hide-dashboard/">IWG Hide Dashboard</a> plugin by Thomas Schneider.
+Description: Simple plugin that removes the Dashboard menu and the Tools menu, and prevents Dashboard access to users assigned to the <em>Subscriber</em> role. Useful if you allow your subscribers to edit their own profiles, but don't want them wandering around your WordPress admin section. Based on the <a title="IWG Hide Dashboard" href="http://www.im-web-gefunden.de/wordpress-plugins/iwg-hide-dashboard/">IWG Hide Dashboard</a> plugin by Thomas Schneider.
 Author: Kim Parsell
 Author URI: http://www.kpdesign.net/
-Version: 1.0
+Version: 1.1
 License: MIT License - http://www.opensource.org/licenses/mit-license.php
 
 Copyright (c) 2008 Kim Parsell
@@ -30,36 +30,63 @@ THE SOFTWARE.
 
 */
 
-if (basename($_SERVER['PHP_SELF']) == basename ( __FILE__ ) ) {
+/* Disallow direct access to the plugin file */
+if (basename($_SERVER['PHP_SELF']) == basename (__FILE__)) {
 die("Sorry, but you can't access this page directly.");
 }
 
-/* Hide dashboard link and redirect unauthorized users to their profile page */
-function wphd_hide_dashboard() {
-	global $menu, $parent_file, $current_user;
+/* Define path to /wp-content/plugins/ */
+if (!defined('WP_CONTENT_URL')) define('WP_CONTENT_URL', get_option('siteurl').'/wp-content');
+if (!defined('WP_CONTENT_DIR')) define('WP_CONTENT_DIR', ABSPATH.'wp-content');
 
-	if ($current_user->wp_capabilities['subscriber'] == 1) {
-		unset($menu[0]);
-		if ($parent_file == 'index.php') {
-			if ( !headers_sent() ) {
-				wp_redirect('profile.php');
-				exit();
-			} else {
-				$wphd_hide_dashboard_url = get_option('siteurl') . "/wp-admin/profile.php";
+if (!defined('WP_PLUGIN_URL')) define('WP_PLUGIN_URL', WP_CONTENT_URL.'/plugins');
+if (!defined('WP_PLUGIN_DIR')) define('WP_PLUGIN_DIR', WP_CONTENT_DIR.'/plugins');
+
+/* Check for current WordPress version */
+function wphd_hide_dashboard_version($test_version) {
+	$wp_version = get_bloginfo('version');
+	return version_compare($test_version, $wp_version);
+}
+
+/* Hide the Dashboard link (2.5+) and the Tools menu (2.7) */
+function wphd_hide_dashboard() {
+	global $menu, $current_user;
+
+	if (!current_user_can('edit_posts')) {
+		if (0 <= wphd_hide_dashboard_version('2.6')) {
+			unset($menu[0]);
+		} else if (0 == wphd_hide_dashboard_version('2.7')) {
+			unset($menu[0]);
+			unset($menu[4]);
+			unset($menu[55]);
+		}
+	}
+}
+
+/* Redirect unauthorized users to their profile page if attempting to access dashboard with direct url */
+function wphd_admin_redirect() {
+	global $parent_file, $current_user;
+
+	if (!current_user_can('edit_posts') && $parent_file == 'index.php') {
+		if (!headers_sent()) {
+			wp_redirect('profile.php');
+			exit();
+		} else {
+			$wphd_hide_dashboard_url = get_option('siteurl') . "/wp-admin/profile.php";
 ?>
 <meta http-equiv="refresh" content="0; url=<?php echo $wphd_hide_dashboard_url; ?>">
 <script type="text/javascript">document.location.href = "<?php echo $wphd_hide_dashboard_url; ?>"</script>
 </head>
-<body>
-</body>
+
+<body></body>
 </html>
 <?php
-				exit();
-			}
+			exit();
 		}
 	}
 }
 
 add_action('admin_head', 'wphd_hide_dashboard', 0);
+add_action('admin_head', 'wphd_admin_redirect', 0);
 
 ?>
