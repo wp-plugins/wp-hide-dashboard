43d04e5545656922c3f69f60a81842d33fa6b35f
kpdesign <kpdesign@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-23 17:13:05 +0000

    = Version 2.1: = * Support for WordPress version 2.9 and 3.0 removed. * Support for roles other than Subscriber removed. * Reworked code for removing Personal Options section on Profile page. * Added support for removing Dashboard links in new admin bar. * Added support for WordPress Multisite (network-activation-capable).
    git-svn-id: https://plugins.svn.wordpress.org/wp-hide-dashboard/trunk@349875 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,33 +1,35 @@
 === WP Hide Dashboard ===
 Contributors: kpdesign
-Donate link: http://kpdesign.net/wp-plugins/wp-hide-dashboard/
-Tags: admin, administration, dashboard, hide
-Requires at least: 2.9
-Tested up to: 3.0
-Stable tag: 2.0
+Donate link: http://kpdesign.net/wphidedash/donate/
+Tags: admin, administration, dashboard, hide, multisite, buddypress
+Requires at least: 3.1
+Tested up to: 3.1
+Stable tag: 2.1
 
-Hide the Dashboard menu, Tools menu, Personal Options section and Help link on the Profile page from your blog subscribers when they are logged in.
+Hide the Dashboard menu, Personal Options section and Help link on the Profile page from your subscribers when they are logged in.
 
 == Description ==
 
-This plugin removes the Dashboard menu, the Tools menu, the Personal Options section and the Help link on the Profile page, and prevents Dashboard access to users assigned to the <em>Subscriber</em> role. Useful if you allow your subscribers to edit their own profiles, but don't want them wandering around your WordPress admin section.
+This plugin removes the Dashboard menu, the Personal Options section and the Help link on the Profile page, hides the Dashboard links in the admin bar menu (if activated), and prevents Dashboard access to users assigned to the <em>Subscriber</em> role. Useful if you allow your subscribers to edit their own profiles, but don't want them wandering around your WordPress admin section.
 
-Users belonging to any of the other WordPress roles will continue to see the Dashboard link, the Tools menu, the Personal Options section and the Help link, and will have access to the other sections of the WordPress admin that corresponds to their role's capabilities.
+Users belonging to any of the other WordPress roles will continue to see and have access to the other sections of the WordPress admin that correspond to their role's capabilities.
 
-Based on the [IWG Hide Dashboard](http://www.im-web-gefunden.de/wordpress-plugins/iwg-hide-dashboard/ "IWG Hide Dashboard") plugin by Thomas Schneider, which requires having the Role Manager plugin activated in order for it to function.
+WP Hide Dashboard has been tested with WordPress in Single user mode, Multisite mode, and with BuddyPress, and works with all of them.
 
-This plugin relies only on core WordPress capabilities.
+<strong>Note: Version 2.1 requires a minimum of WordPress 3.1. If you are running a version less than that, please upgrade your WordPress install before installing or upgrading.</strong>
 
 = Works With: =
 
-The following is a list of role/content management plugins that work well (no conflicts) with the WP Hide Dashboard plugin:
+The following is a list of plugins that work well (no conflicts) with the WP Hide Dashboard plugin:
 
 * [Members](http://wordpress.org/extend/plugins/members/ "Members") by Justin Tadlock
-* [wpNamedUsers](http://wordpress.sundskard.dk/archives/category/wpnamedusers "wpNamedUsers") by Andrias Sundskard
+* [BuddyPress](http://wordpress.org/extend/plugins/buddypress/ "BuddyPress") by the BuddyPress team
+* [Admin Bar Minimiser](http://wordpress.org/extend/plugins/admin-bar-minimiser/ "Admin Bar Minimiser") by David Gwyer
+* [Logged Out Admin Bar](http://wordpress.org/extend/plugins/logged-out-admin-bar/ "Logged Out Admin Bar") by Peter Westwood
 
 = Known Conflicts: =
 
-The following is a list of role/content management plugins that are known to have conflicts with the WP Hide Dashboard plugin:
+The following is a list of plugins that are known to have conflicts with the WP Hide Dashboard plugin:
 
 * [Role Manager](http://www.im-web-gefunden.de/wordpress-plugins/role-manager/ "Role Manager") (Use the [IWG Hide Dashboard](http://www.im-web-gefunden.de/wordpress-plugins/iwg-hide-dashboard/ "IWG Hide Dashboard") plugin to hide the dashboard link.)
 * [Role Scoper](http://wordpress.org/extend/plugins/role-scoper/ "Role Scoper")
@@ -37,28 +39,44 @@ Note: Please let me know if there are other plugins that conflict with WP Hide D
 
 = Support: =
 
-Support is provided at: http://kpdesign.net/wp-plugins/wp-hide-dashboard/
+Support is provided at: http://kpdesign.net/wphidedash/
 
 == Installation ==
 
 = Installation Instructions: =
 
+This plugin works in either the `/wp-content/plugins/` or the `/wp-content/mu-plugins/` directory.
+
+If installing the plugin in the `/wp-content/plugins/` directory:
+
 1. Download the plugin and unzip it to a folder on your computer.
-2. Upload the `wp-hide-dashboard` folder to the `wp-content/plugins/` directory.
+2. Upload the entire `wp-hide-dashboard` folder to that directory.
 3. Activate the plugin through the Plugins section in WordPress.
-4. That's it! There is no configuration necessary.
+4. That's it - no configuration necessary.
+
+If installing the plugin in the `/wp-content/mu-plugins/` directory:
+
+1. Download the plugin and unzip it to a folder on your computer.
+2. Upload the wp-hide-dashboard.php file to the `/wp-content/mu-plugins/` directory.
+3. The plugin will automatically run – there is no activation or configuration needed.
 
 == Frequently Asked Questions ==
 
-Below are a couple of the most frequently asked questions.
+**Q. Why did you remove support for other roles from Version 2.1?**
 
-Check out http://kpdesign.net/wp-plugins/wp-hide-dashboard/ for the full FAQ.
+I removed support for menu items for roles other than Subscriber. WP Hide Dashboard is a simple plugin, and was built for the Subscriber role. While I appreciate that some users need this type of functionality for other roles, most don't require it, so I don't want to bloat the plugin with unnecessary code for the majority of users.
+
+If you need to hide admin sidebar menu links from roles other than Subscriber, I recommend using the [Admin Menu Editor plugin](http://wordpress.org/extend/plugins/admin-menu-editor/ "Admin Menu Editor plugin") to accomplish that goal. Currently it only removes links in the admin sidebar menu. The plugin author has indicated they may add support for removing admin bar menu links in a future version.
 
 **Q. How do I change this to hide the Dashboard link, Tools menu, Personal Options, and Help options from other roles besides Subscriber?**
 
 A. To hide these from other roles, you will need to edit the plugin in a plain text editor and make the following changes:
 
-**Version 1.5 and up:**
+**Version 2.1:**
+
+Support for removing other admin menu links has been removed from this version (see response to question above).
+
+**Version 1.5 and 2.0:**
 
 You will need to change the capability (line 44 in version 1.5, and line 46 in version 2.0):
 
@@ -77,16 +95,23 @@ There are 3 instances of this code in the plugin - make sure you change all of t
 * Subscriber -> Author: Change `!current_user_can('edit_posts')` to `!current_user_can('manage_categories')`
 * Subscriber -> Editor: Change `!current_user_can('edit_posts')` to `!current_user_can('manage_options')`
 
-**Q. Will you be creating an admin option page to allow specifying what role we want to hide these from?**
-
-A. I have this on the to-do list for the plugin for a future release. In the meantime, you will still need to edit the plugin manually.
-
 == Screenshots ==
 
-1. WordPress 3.0 default Subscribers profile page
-2. WordPress 3.0 Subscribers profile page with WP Hide Dashboard activated
+1. WordPress 3.1 default Subscribers profile page (single user - admin bar dropdown showing)
+2. WordPress 3.1 Subscribers profile page with WP Hide Dashboard activated (single user - admin bar dropdown showing)
+3. WordPress 3.1 default Subscribers profile page (Multisite - admin bar showing)
+4. WordPress 3.1 Subscribers profile page with WP Hide Dashboard activated (Multisite - admin bar showing)
+5. WordPress 3.1 website (Multisite - admin bar dropdown showing)
+6. WordPress 3.1 website with WP Hide Dashboard activated (Multisite - admin bar dropdown showing)
 
 == Changelog ==
+
+= Version 2.1: =
+* Support for WordPress version 2.9 and 3.0 removed.
+* Support for roles other than Subscriber removed.
+* Reworked code for removing Personal Options section on Profile page.
+* Added support for removing Dashboard links in new admin bar.
+* Added support for WordPress Multisite (network-activation-capable).
 
 = Version 2.0: =
 * Code reworked; support for WordPress version 2.5 - 2.8 removed.
@@ -96,7 +121,7 @@ A. I have this on the to-do list for the plugin for a future release. In the mea
 = Version 1.5: =
 * Added code to make it easier to configure plugin if you want to change the role/capability level.
 * Added code to remove Tools menu.
-* Added code to remove Settings, Media and Comments menus for Contributors+ if needed
+* Added code to remove Settings, Media and Comments menus for Contributors+ if needed.
 * Added code to remove WordPress upgrade nag notice (admin will still see the notice).
 
 = Version 1.4: =
@@ -119,9 +144,5 @@ A. I have this on the to-do list for the plugin for a future release. In the mea
 
 == Upgrade Notice ==
 
-= Version 2.0: =
-**Note: This plugin now requires a minimum of WordPress 2.9. If you are running a version less than that, please upgrade your WordPress install now, then upgrade to the latest version of the plugin.**
-
-* Code reworked; support for WordPress version 2.5 - 2.8 removed.
-* Updated menu removal code for compatibility with WordPress 3.0 (single user mode).
-* Added code to remove Personal Options section on Profile page (props to Matthew Pollotta).
+= Version 2.1: =
+See changelog for full list of changes: http://wordpress.org/extend/plugins/wp-hide-dashboard/changelog/. Note: This version requires a minimum of WordPress 3.1. If you are running a version less than that, upgrade your WordPress install now, then upgrade to the latest version of the plugin.
--- wp-hide-dashboard.php
+++ wp-hide-dashboard.php
@@ -1,16 +1,16 @@
 <?php
 /*
 Plugin Name: WP Hide Dashboard
-Plugin URI: http://kpdesign.net/wp-plugins/wp-hide-dashboard/
-Description: A simple plugin that removes the Dashboard menu, the Tools menu, the Personal Options section and the Help link on the Profile page, and prevents Dashboard access to users assigned to the <em>Subscriber</em> role. Useful if you allow your subscribers to edit their own profiles, but don't want them wandering around your WordPress admin section. Note: This version requires a minimum of WordPress 2.9. If you are running a version less than that, please upgrade your WordPress install now.
+Plugin URI: http://kpdesign.net/wphidedash/
+Description: A simple plugin that removes the Dashboard menu, the Personal Options section and the Help link on the Profile page, hides the Dashboard links in the admin bar menu (if activated), and prevents Dashboard access to users assigned to the <em>Subscriber</em> role. Useful if you allow your subscribers to edit their own profiles, but don't want them wandering around your WordPress admin section. <strong>Note: This version requires a minimum of WordPress 3.1. If you are running a version less than that, please upgrade your WordPress install now.</strong>
 Author: Kim Parsell
 Author URI: http://kpdesign.net/
-Version: 2.0
+Version: 2.1
 License: MIT License - http://www.opensource.org/licenses/mit-license.php
 
-Copyright (c) 2008-2010 Kim Parsell
+Copyright (c) 2008-2011 Kim Parsell
 Personal Options removal code: Copyright (c) 2010 Large Interactive, LLC, Author: Matthew Pollotta
-Based on IWG Hide Dashboard plugin by Thomas Schneider, Copyright (c) 2008 (http://www.im-web-gefunden.de/wordpress-plugins/iwg-hide-dashboard/)
+Originally based on IWG Hide Dashboard plugin by Thomas Schneider, Copyright (c) 2008 (http://www.im-web-gefunden.de/wordpress-plugins/iwg-hide-dashboard/)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this
 software and associated documentation files (the "Software"), to deal in the Software
@@ -35,71 +35,110 @@ if (basename($_SERVER['PHP_SELF']) == basename (__FILE__)) {
 	die('Sorry, but you cannot access this page directly.');
 }
 
-/* Define path to /wp-content/plugins/ */
-if (!defined('WP_CONTENT_URL')) define('WP_CONTENT_URL', get_option('siteurl').'/wp-content');
-if (!defined('WP_CONTENT_DIR')) define('WP_CONTENT_DIR', ABSPATH.'wp-content');
-
-if (!defined('WP_PLUGIN_URL')) define('WP_PLUGIN_URL', WP_CONTENT_URL.'/plugins');
-if (!defined('WP_PLUGIN_DIR')) define('WP_PLUGIN_DIR', WP_CONTENT_DIR.'/plugins');
-
 /* Plugin config - user capability for the top level you want to hide everything from */
 $wphd_user_capability = 'edit_posts'; /* [default for Subscriber role = edit_posts] */
 
-/* Hide the Dashboard link, Help menu, Favorites menu, Upgrade notice, and now the Personal Options section too!
-	Major thanks to Matt Pollotta of Large Interactive for allowing me to incorporate the functionality from his Hide Personal Options plugin
-	(http://matt.largeinteractive.com/removing-personal-options-from-wordpress/28/) into WP Hide Dashboard. More features FTW! */
+/* WordPress 3.1 introduces the admin bar in both the admin area and the public-facing site. For subscribers, there's also now
+	a pesky link to the Dashboard and a redundant link to the user's profile in the My Account menu. Let's remove the Dashboard
+	link and only show the Profile link on the site. */
+
+function wphd_remove_admin_bar_links() {
+	global $blog, $current_user, $id, $wp_admin_bar, $wphd_user_capability;
+
+	if ((!current_user_can(''.$wphd_user_capability.'') || !current_user_can_for_blog($blog->userblog_id, ''.$wphd_user_capability.'')) && is_admin_bar_showing()) {
+
+		/* If Multisite, check whether they are assigned to any network sites before removing links */
+		$user_id = get_current_user_id();
+		$blogs = get_blogs_of_user($user_id);
+		if (is_multisite() && empty($blogs)) {
+			return;
+		} else if (is_admin() || is_blog_admin()) {
+			$wp_admin_bar->remove_menu('edit-my-profile');
+		}
+		$wp_admin_bar->remove_menu('dashboard');
+
+	}
+
+}
+
+add_action('admin_bar_menu', 'wphd_remove_admin_bar_links', 100);
+
+/* There's also an admin bar menu in WordPress Multisite that lists all of the network sites that a user belongs to, which includes links
+	to each site's Dashboard. Let's remove the default menu and create one of our own that only links to the sites a user belongs to. */
+
+function wphd_custom_my_sites_menu() {
+	global $blog, $current_user, $wpdb, $wp_admin_bar, $wphd_user_capability;
+
+	if (is_multisite()) {
+		if (!current_user_can_for_blog($blog->userblog_id, ''.$wphd_user_capability.'') && is_admin_bar_showing() && is_user_logged_in()) {
+			/* Add custom My Sites menu if the user is assigned to one or more sites. */
+			if (count($wp_admin_bar->user->blogs) < 1)
+				return;
+
+			$wp_admin_bar->add_menu(array('id' => 'my-blogs', 'title' => __('My Sites'), 'href' => $null));
+
+			$default = includes_url('images/wpmini-blue.png');
+
+			foreach ((array) $wp_admin_bar->user->blogs as $blog) {
+				$blavatar = '<img src="'.esc_url($default).'" alt="'.esc_attr__('Blavatar').'" width="16" height="16" class="blavatar" />';
+				$blogname = empty( $blog->blogname ) ? $blog->domain : $blog->blogname;
+				$wp_admin_bar->add_menu(array('parent' => 'my-blogs', 'id' => 'blog-'.$blog->userblog_id, 'title' => $blavatar.$blogname, 'href' => get_home_url($blog->userblog_id)));
+			}
+		}
+
+	}
+
+}
+
+remove_action('admin_bar_menu', 'wp_admin_bar_my_sites_menu', 90);
+add_action('admin_bar_menu', 'wphd_custom_my_sites_menu', 100);
+
+/* Now for the admin sidebar menu and the profile page. Let's hide the Dashboard link, Help menu, Favorites menu, Upgrade notice, and
+	the Personal Options section. */
+
+function wphd_hide_dashboard() {
+	global $blog, $current_user, $id, $parent_file, $wphd_user_capability, $wp_db_version;
+
+	if ($wp_db_version < 17056) {
+		return;
+
+	} else if ((!current_user_can(''.$wphd_user_capability.'') || !current_user_can_for_blog($blog->userblog_id, ''.$wphd_user_capability.'')) && $wp_db_version >= 17056) {
+
+		/* First, let's get rid of the Help menu, update nag, Personal Options section */
+		echo "\n" . '<style type="text/css" media="screen">#your-profile { display: none; } .update-nag, #screen-meta, .color-option, .show-admin-bar { display: none !important; }</style>';
+		echo "\n" . '<script type="text/javascript">jQuery(document).ready(function($) { $(\'form#your-profile > h3:first\').hide(); $(\'form#your-profile > table:first\').hide(); $(\'form#your-profile\').show(); });</script>' . "\n";
+
+		/* Now, let's fix the sidebar admin menu - go away, Dashboard link. */
+		/* If Multisite, check whether they are in the User Dashboard before removing links */
+		$user_id = get_current_user_id();
+		$blogs = get_blogs_of_user($user_id);
+		if (is_multisite() && empty($blogs)) {
+			return;
+		} else if (function_exists('remove_menu_page')) {
+			remove_menu_page('index.php');							/* Hides Dashboard menu in 3.1 */
+			remove_menu_page('separator1');							/* Hides arrow separator under Dashboard link in 3.1*/
+			remove_submenu_page('profile.php', 'profile.php');		/* Hides Profile submenu link in 3.1. Really don't need to see it twice, do we? */
+		}
+
+		/* Last, but not least, let's redirect folks to their profile when they login or if they try to access the Dashboard via direct URL */
+		if (is_multisite() && empty($blogs)) {
+			return;
+		} else if ($parent_file == 'index.php') {
+			if (headers_sent()) {
+				echo '<meta http-equiv="refresh" content="0;url='.admin_url('profile.php').'">';
+				echo '<script type="text/javascript">document.location.href="'.admin_url('profile.php').'"</script>';
+			} else {
+				wp_redirect(admin_url('profile.php'));
+				exit();
+			}
+		}
+
+	}
+
+}
 
 add_action('admin_head', 'wphd_hide_dashboard', 0);
 
-function wphd_hide_dashboard() {
-	global $current_user, $menu, $parent_file, $wphd_user_capability, $wp_db_version;
-
-	/* First, let's get rid of the Help menu, update nag, Turbo nag, Favorites menu, Personal Options section */
-	if (!current_user_can(''.$wphd_user_capability.'')) {
-
-		/* For folks still on 2.9/2.9.1/2.9.2. You really should upgrade, you know that, right? */
-		if ($wp_db_version === 12329) { /* This is WordPress 2.9, 2.9.1, 2.9.2 */
-			echo "\n" . '<style type="text/css" media="screen">#your-profile { display: none; } #update-nag, #screen-meta, .turbo-nag, #favorite-actions { display: none !important; }</style>';
-			echo "\n" . '<script type="text/javascript" src="'.WP_PLUGIN_URL.'/wp-hide-dashboard/wphd-hpo.js"></script>' . "\n";
-		}
-
-		/* For folks running 3.0 or higher, some leaner code. Don't need to remove Turbo link or Favorites menu any longer.
-			Gears support and Turbo link were removed in changeset 11301: http://core.trac.wordpress.org/ticket/11301.
-			Favorites menu is hidden by default for Subscribers in version 3.0. */
-		else if ($wp_db_version >= 15260) { /* This is WordPress 3.0 */
-			echo "\n" . '<style type="text/css" media="screen">#your-profile { display: none; } .update-nag, #screen-meta { display: none !important; }</style>';
-			echo "\n" . '<script type="text/javascript" src="'.WP_PLUGIN_URL.'/wp-hide-dashboard/wphd-hpo.js"></script>' . "\n";
-		}
-
-	/* Now, let's fix the admin menu. */
-
-		/* First let's take care of the actual Dashboard link. The Dashboard link menu numbers are different in 2.9.x and 3.0. */
-		if ($wp_db_version === 12329) { /* This is WordPress 2.9, 2.9.1, 2.9.2 */
-			unset($menu[0]);		/* Hides Dashboard menu in 2.9.x and under*/
-		} else if ($wp_db_version >= 15260) { /* This is WordPress 3.0 */
-			unset($menu[2]);		/* Hides Dashboard menu in 3.0 */
-		}
-
-		/* Now on to the rest of the menu. */
-		unset($menu[4]);		/* Hides arrow separator under Dashboard link */
-		unset($menu[10]);		/* Hides Media menu (contributors +) */
-		unset($menu[25]);		/* Hides Comments menu (contributors +) */
-		unset($menu[75]);		/* Hides Tools menu in 2.9.x. This is now hidden from Subscribers by default in 3.0, courtesy of changeset 11301 (thanks @nacin). */
-		unset($menu[80]);		/* Hides Settings menu (contributors +) */
-		unset($menu[9999]);	/* Hides top-level menu for Pods plugin */
-	}
-
-	/* Last, but not least, let's redirect folks to their profile when they login or if they try to access the dashboard via direct URL */
-
-	if (!current_user_can(''.$wphd_user_capability.'') && $parent_file == 'index.php') {
-		if (headers_sent()) {
-			echo '<meta http-equiv="refresh" content="0;url='.admin_url('profile.php').'">';
-			echo '<script type="text/javascript">document.location.href="'.admin_url('profile.php').'"</script>';
-		} else {
-			wp_redirect(admin_url('profile.php'));
-		}
-	}
-
-} /* That's all folks. You were expecting more? */
+/* That's all folks. You were expecting more? */
 
 ?>
